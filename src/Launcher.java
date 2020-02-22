f2f24df930685b70c42a8f6b545ed529cbc82b7d
Andrey Lashenko <andreylashenko@mail.ru>
2020-02-22 23:10:55 +0300

    rename game title

--- src/Launcher.java
+++ src/Launcher.java
@@ -3,7 +3,7 @@ import settings.Settings;
 
 public class Launcher {
     public static void main(String[] args) {
-        Game game = new Game("2D_Game", Settings.GAME_WIDTH, Settings.GAME_HEIGHT);
+        Game game = new Game("Arkanoid game", Settings.GAME_WIDTH, Settings.GAME_HEIGHT);
         game.start();
     }
 }
